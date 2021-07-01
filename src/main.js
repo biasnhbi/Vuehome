b02c9bb6312ff44cc2442baeb03d77fde9597239
biasnhbi <61118208+biasnhbi@users.noreply.github.com>
2021-07-01 23:42:05 +0800

    test

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import './assets/fonts/iconfont.css'
 import axios from 'axios'
 
 Vue.config.productionTip = false
-axios.defaults.baseURL=`http://127.0.0.1:8888/api/private/v1/`
+axios.defaults.baseURL=`https://lianghj.top:8888/api/private/v1/`
 axios.interceptors.request.use(config => {
 // Do something before request is sent
 config.headers.Authorization=window.sessionStorage.getItem('token')
